111cc9a9182a9d55d1bcfccfbd3913642128f880
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-02-11 11:01:06 +0800

    Add CORS header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const Koa = require('koa');
 const _ = require('koa-route');
 const cache = require('koa-cache-lite');
+const cors = require('@koa/cors');
 const api = require('./api');
 const config = require('./config');
 
 const app = new Koa();
+
+app.use(cors());
 
 cache.configure({
   '/api/*': 3600000, // Cache responses for an hour
